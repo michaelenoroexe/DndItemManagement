e5fb51148f671e6e0260c9e315ab684fa479536c
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-11-15 00:13:32 +0300

    Configurated angular proxy.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -2,14 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddMvcCore();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,11 +16,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
+//app.UseMvc();
+app.MapControllers();
 
 app.Run();
